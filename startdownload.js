b3aa522dfa214754193c9c46d5453019ba0ec26e
Howie <johnson.hyz@gmail.com>
2017-11-13 16:56:56 +0800

    加入判断，如果为Chinese则下载，解决Chrome只弹出一个的问题，而且大家一般都是下载中文

--- startdownload.js
+++ startdownload.js
@@ -6,7 +6,8 @@ var tracks = document.getElementsByTagName('track');//$('track');
 // alert(tracks.length);
 for (var i = 0; i < tracks.length; i++) {
   var t = tracks[i];
-  var a = document.createElement('a');
+  if (t.label == 'Chinese') {
+  	var a = document.createElement('a');
   // alert(t.src+"da");
   var href = t.src;
   a.href = href;
@@ -14,4 +15,6 @@ for (var i = 0; i < tracks.length; i++) {
   var len = tokens.len;
   a.download = ('heihei');// TODO: could not name downloaded file, fix this
   a.click();
+  }
+  
 }
